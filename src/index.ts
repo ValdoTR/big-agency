44bc0bd52649fb680396105ef90c2be3b6bbf9a1
ValdoTR <vteixeiraromao@gmail.com>
2022-01-21 14:51:20 +0000

    Deploying to gh-pages from master @ aff0dfc151ecd7d8ca54961561b1ed1a1c337c36 🚀

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,50 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
-
-import {bootstrapExtra} from "@workadventure/scripting-api-extra";
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
 
-let currentPopup: any = undefined;
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
+console.log('Script started successfully');
 
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
+// Manage popups
+let currentZone: string;
+let currentPopup: any;
+
+WA.room.onEnterLayer('exitWest').subscribe(() => {
+    currentZone = 'exitWest'
+    const isDoorConfigured = WA.state.loadVariable(currentZone + 'URL')
+    if (isDoorConfigured) return;
+    openExitPopup()
 })
+WA.room.onLeaveLayer('exitWest').subscribe(closePopup)
 
-WA.room.onLeaveZone('clock', closePopUp)
+WA.room.onEnterLayer('exitEast').subscribe(() => {
+    currentZone = 'exitEast'
+    const isDoorConfigured = WA.state.loadVariable(currentZone + 'URL')
+    if (isDoorConfigured) return;
+    openExitPopup()
+})
+WA.room.onLeaveLayer('exitEast').subscribe(closePopup)
 
-function closePopUp(){
+// Popup management functions
+function openExitPopup(): void {
+    const popupName = currentZone + 'Popup'
+    const variableName = currentZone + 'URL'
+
+    let cta = []
+    if (WA.player.tags.includes('editor')) {
+        cta.push({
+            label: 'Configure',
+            className: 'primary',
+            callback: () => WA.nav.openCoWebSite('https://workadventu.re')
+        })
+    }
+    // TODO: add WA.nav.openConfig(variableName)
+
+    // @ts-ignore otherwise we can't assign cta variable
+    currentPopup = WA.ui.openPopup(popupName, 'This exit is not configured yet.', cta)
+}
+function closePopup(): void {
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
