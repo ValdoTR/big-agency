d0b907cba9ea3b992f60bb170ef52bf94e3542e2
ValdoTR <vteixeiraromao@gmail.com>
2022-01-26 16:29:07 +0000

    Deploying to gh-pages from master @ c2ce7d171b6844eaece026257a30d385e6c4d1c7 🚀

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,21 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-// The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
-bootstrapExtra().catch(e => console.error(e));
-
 console.log('Script started successfully');
 
-// Manage popups
-let currentZone: string;
-let currentPopup: any;
-
-WA.room.onEnterLayer('exitWest').subscribe(() => {
-    currentZone = 'exitWest'
-    const isDoorConfigured = WA.state.loadVariable(currentZone + 'URL')
-    if (isDoorConfigured) return;
-    openExitPopup()
-})
-WA.room.onLeaveLayer('exitWest').subscribe(closePopup)
-
-WA.room.onEnterLayer('exitEast').subscribe(() => {
-    currentZone = 'exitEast'
-    const isDoorConfigured = WA.state.loadVariable(currentZone + 'URL')
-    if (isDoorConfigured) return;
-    openExitPopup()
-})
-WA.room.onLeaveLayer('exitEast').subscribe(closePopup)
-
-// Popup management functions
-function openExitPopup(): void {
-    const popupName = currentZone + 'Popup'
-    const variableName = currentZone + 'URL'
-
-    let cta = []
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
+    
+    // Show configuration tile for editors only
     if (WA.player.tags.includes('editor')) {
-        cta.push({
-            label: 'Configure',
-            className: 'primary',
-            callback: () => WA.nav.openCoWebSite('https://workadventu.re')
-        })
+        WA.room.showLayer('exitNorthConfig')
+        WA.room.showLayer('exitWestConfig')
     }
-    // TODO: add WA.nav.openConfig(variableName)
 
-    // @ts-ignore otherwise we can't assign cta variable
-    currentPopup = WA.ui.openPopup(popupName, 'This exit is not configured yet.', cta)
-}
-function closePopup(): void {
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+}).catch(e => console.error(e));
